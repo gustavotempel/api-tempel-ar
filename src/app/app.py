97b202c18474edb805cc4d253e176e6c5df1f8ab
Gustavo Tempel <gtempel@hotmail.com>
2021-08-18 21:03:02 -0300

    Custom response added to endpoint

--- src/app/app.py
+++ src/app/app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return "Hello World!"
+    return custom_response("Hello World!")
+
+def custom_response(body, code=None, headers=None):
+    """Returns a tuple with body, code and/or headers"""
+    if code is None:
+        code = 200
+    if headers is None:
+        headers = {
+            "Access-Control-Allow-Origin": "*"
+            }
+    return body, code, headers
 
 if __name__ == "__main__":
     app.run(debug=True)
