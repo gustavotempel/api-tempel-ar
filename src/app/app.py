70fffca6428f2682c5e99ef8f2dacd77110bbe72
Gustavo Tempel <gtempel@hotmail.com>
2021-08-19 00:14:07 -0300

    Content endpoint for testing

--- src/app/app.py
+++ src/app/app.py
@@ -7,6 +7,26 @@ app = Flask(__name__)
 def hello_world():
     return custom_response("Hello World!")
 
+@app.route("/content", methods=["POST"])
+def content():
+    return custom_response(
+        {
+        "order": ["Skills", "Profile", "Experience"],
+        "data": [
+            {
+                "title": "Profile",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ad unde adipisci quas veniam doloribus nobis alias qui, veritatis numquam eligendi sed tempora odio officia commodi non consectetur. Provident, assumenda.",
+            },
+            {
+                "title": "Experience",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ad unde adipisci quas veniam doloribus nobis alias qui, veritatis numquam eligendi sed tempora odio officia commodi non consectetur. Provident, assumenda.",
+            },
+            {
+                "title": "Skills",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ad unde adipisci quas veniam doloribus nobis alias qui, veritatis numquam eligendi sed tempora odio officia commodi non consectetur. Provident, assumenda.",
+            }
+        ]}, 200)
+
 def custom_response(body, code=None, headers=None):
     """Returns a tuple with body, code and/or headers"""
     if code is None:
