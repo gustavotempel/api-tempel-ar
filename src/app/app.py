9d7a6de8fa71428d266051f13a14dfb5465a5de1
Gustavo Tempel <gtempel@hotmail.com>
2021-08-24 00:05:42 -0300

    CRUD methods added

--- src/app/app.py
+++ src/app/app.py
@@ -1,11 +1,62 @@
 from flask import Flask
+from flask import jsonify
+from flask import request
+
+from database import select_query
+from database import modify_query
 
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
     return custom_response("Hello World!")
+
+
+@app.route("/products", methods=["POST"])
+def add_product():
+    fields = ["name", "price", "url"]
+    product_to_add = request.json
+    values = ", ".join(str(add_single_quotes_to_str(product_to_add[field])) for field in fields)
+    products_query = modify_query(f"INSERT INTO products ({', '.join(fields)}) VALUES ({values})")
+    return custom_response("Product added")
+
+
+@app.route("/products", methods=["GET"])
+def get_products():
+    fields = ["id", "name", "price", "url"]
+    products_query = select_query(f"SELECT {', '.join(fields)} FROM products")
+    products = list(filter(None, products_query)) if products_query else []
+    product_list = []
+    for product in products:
+        product_list.append(dict(zip(fields, product)))
+    return custom_response(jsonify(product_list))
+
+
+@app.route("/products/<id>", methods=["GET"])
+def get_product_by_id(id):
+    fields = ["id", "name", "price", "url"]
+    product_query = select_query(f"SELECT {', '.join(fields)} FROM products WHERE id={id}")
+    product = dict(zip(fields, product_query[0])) if product_query else None
+    return custom_response(jsonify(product))
+
+
+@app.route("/products/<id>", methods=["DELETE"])
+def delete_product_by_id(id):
+    product_query = modify_query(f"DELETE FROM products WHERE id={id}")
+    return custom_response("Product deleted")
+
+
+@app.route("/products/<id>", methods=["PUT"])
+def put_product_by_id(id):
+    fields = ["name", "price", "url"]
+    product_to_put = request.json
+    values = ", ".join(str(add_single_quotes_to_str(product_to_put[field])) for field in fields)
+    field_values = ", ".join(field + "=" + str(add_single_quotes_to_str(product_to_put[field])) for field in fields)
+    product_query = modify_query(f"UPDATE products SET {field_values} WHERE id={id}")
+    return custom_response("Product completely updated")
+
 
 def custom_response(body, code=None, headers=None):
     """Returns a tuple with body, code and/or headers"""
@@ -16,6 +67,13 @@ def custom_response(body, code=None, headers=None):
             "Access-Control-Allow-Origin": "*"
             }
     return body, code, headers
+
+
+def add_single_quotes_to_str(value):
+    if isinstance(value, str):
+        return f"'{value}'"
+    return value
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/app/database.py
@@ -0,0 +1,52 @@
+
+import os
+import psycopg2
+
+
+def retrieve_database_config():
+    """
+    Returns an environment variable corresponding to the database configuration in the following format:
+    postgres://{user}:{password}@{hostname}:{port}/{database-name}
+    """
+    try:
+        config = os.environ['DATABASE_URL']
+    except:
+        pass
+    return config
+
+
+DB_CONFIG = retrieve_database_config()
+
+
+def select_query(query, config=DB_CONFIG):
+    cursor = None
+    try:
+        connection = psycopg2.connect(config, sslmode="require")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Exception as error:
+        print("Error connecting to data base.", error)
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+
+def modify_query(query, config=DB_CONFIG):
+    cursor = None
+    try:
+        connection = psycopg2.connect(config, sslmode="require")
+        cursor = connection.cursor()
+        cursor.execute(query)
+        connection.commit()
+    except Exception as error:
+        print("Error connecting to data base.", error)
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
